b871fe116004921ffabf3287f0362919b2d9448f
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2024-02-15 21:42:33 +0000

    Added javascript file with json data

---
+++ vue_task_1/staticfiles/js/script.js
@@ -0,0 +1,26 @@
+var dinos = [
+    {
+        name: "Littlefoot", 
+        type: "Brontosaurus", 
+        age: 145500000, 
+        isAlive: false
+    },
+    {
+        name: "Steve", 
+        type: "Operations Director", 
+        age: 44, 
+        isAlive: true
+    },
+    {
+        name: "Barry", 
+        type: "Release Dinosaur", 
+        age: 44, 
+        isAlive: false
+    },
+    {
+        name: "Non-Trademark-Violating-Zilla", 
+        type: "Godlike", 
+        age: 64, 
+        isAlive: false
+    }
+];
