524d350e49ee5639786d7aa84db2282e5d5d5e58
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2024-02-17 23:51:55 +0000

    Used visualisation class to plot both data on main

--- visualising_data_task/main.py
+++ visualising_data_task/main.py
@@ -1,2 +1,13 @@
 from visualiser import DataVisualisation
 from data import drink_data, sweet_data
+
+def main():
+    plot_sweets = DataVisualisation(sweet_data, 'Sweet', 'Mass (g)', 'Estimated mass of sweets consumed during a student hackathon.')
+    plot_drinks = DataVisualisation(drink_data, 'Drink', 'Litre', 'Estimated litres of soft drink consumed at a student hackathon.')
+    
+    plot_drinks.plot_data()
+
+    plot_sweets.plot_data()
+    
+if __name__ == "__main__":
+    main()
