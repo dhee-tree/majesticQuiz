62b6813fd73a168c1d85cf73a1e3c8900208ccec
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2024-02-18 02:31:41 +0000

    Created data file with required data

---
+++ django_visualisation/MajesticDataProject/MainApp/data.py
@@ -0,0 +1,14 @@
+sweet_data = {
+    'Teacakes': 600,
+    'Lollipops': 700,
+    'Parma Violets': 3000,
+    'Chocolate': 500,
+}
+
+drink_data = {
+    'Cola': 24,
+    'Diet Cola': 20,
+    'Lemonade': 19,
+    'Dandelion and Burdock': 60,
+    'Orangeade': 10,
+}
