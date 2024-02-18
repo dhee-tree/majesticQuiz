a13ba53fa78f3b6eb73061254ae40b16d9d7257c
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2024-02-18 02:20:12 +0000

    Modified visualisation class to save image to staticfiles image folder

--- django_visualisation/MajesticDataProject/MainApp/utils.py
+++ django_visualisation/MajesticDataProject/MainApp/utils.py
@@ -1,16 +1,18 @@
 import matplotlib.pyplot as plt
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 class DataVisualisation:
     """
     A class to visualise data. Takes a dictionary of data, x and y labels and a title.
     """
+
     def __init__(self, data, x_label, y_label, title):
         self.data = data
         self.x_label = x_label
         self.y_label = y_label
         self.title = title
 
-    def plot_data(self):
+    def plot_data(self, filename):
         """Method to plot the data"""
         data_names = list(self.data.keys())
         data_amount = list(self.data.values())
@@ -26,4 +28,5 @@ class DataVisualisation:
         ax.set_xlabel(self.x_label)
         ax.set_ylabel(self.y_label)
         plt.title(self.title)
-        plt.show()
+        plt.savefig(staticfiles_storage.path(f'image/{filename}.png'))
+        plt.close()
