6707f22f363cdded0daaaab1233ebddaad90700f
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2024-02-19 11:24:54 +0000

    Added view data url, view and template

--- django_visualisation/MajesticDataProject/MainApp/urls.py
+++ django_visualisation/MajesticDataProject/MainApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='index'),
+    path('view-data/<str:data>/', views.ViewData.as_view(), name='view_data'),
 ]
--- django_visualisation/MajesticDataProject/MainApp/views.py
+++ django_visualisation/MajesticDataProject/MainApp/views.py
@@ -1,5 +1,7 @@
 from django.views import View
 from django.shortcuts import render
+from .utils import DataVisualisation
+from .data import sweet_data, drink_data
 
 # Create your views here.
 
@@ -9,3 +11,24 @@ class HomePageView(View):
 
     def get(self, request):
         return render(request, self.template_name)
+
+
+class ViewData(View):
+    template_name = 'data/view_data.html'
+
+    def get(self, request, data):
+        if data == 'sweet':
+            data_vis = DataVisualisation(sweet_data, 'Sweets', 'Amount', 'Sweet Data')
+            data_vis.plot_data('sweet_data.png')
+            filename = 'sweet_data.png'
+        elif data == 'drink':
+            data_vis = DataVisualisation(drink_data, 'Drinks', 'Amount', 'Drink Data')
+            data_vis.plot_data('drink_data.png')
+            filename = 'drink_data.png'
+        else:
+            return render(request, '404.html')
+
+        context = {
+            'file': filename,
+        }
+        return render(request, self.template_name, context)
