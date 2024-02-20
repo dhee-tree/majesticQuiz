e4c847745f4ac1e128c59a3225743164565ac407
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2024-02-20 22:00:26 +0000

    Added upload data view post method which renders data

--- UploadDataApp/views.py
+++ UploadDataApp/views.py
@@ -12,4 +12,29 @@ class UploadDataView(View):
     
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {'form': self.form})
-        
+
+    def post(self, request, *args, **kwargs):
+        post_form = self.form(request.POST, request.FILES)
+
+        if post_form.is_valid():
+            uploaded_file = post_form.cleaned_data['file']
+
+            fs = FileSystemStorage()
+            filename = fs.save(uploaded_file.name, uploaded_file)
+
+            saved_file = fs.path(filename)
+
+            renderfile = RenderUploadFileHTML(saved_file)
+
+            bar = renderfile.render()
+
+            messages.success(request, 'File uploaded successfully')
+            context = {
+                'form': self.form,
+                'bar': bar,
+            }
+
+            return render(request, self.template_name, context)
+        else:
+            messages.error(request, 'File upload failed. Please try again')
+            return render(request, self.template_name, {'form': self.form})
