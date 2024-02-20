341d8ffcb44e0f42783f15084fc677dcf1b2f07c
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2024-02-20 21:32:28 +0000

    Added upload data view

--- UploadDataApp/views.py
+++ UploadDataApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib import messages
+from .utils import RenderUploadFileHTML
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
+
+class UploadDataView(View):
+    form = None
+    template_name = 'data/upload_data.html'
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'form': self.form})
+        
