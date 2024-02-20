c2a79bb20385ccd38a9d05ebd4701b6bc489b14e
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2024-02-20 16:28:36 +0000

    Added custom styling to html figure returned by utils

--- MainApp/utils.py
+++ MainApp/utils.py
@@ -24,4 +24,14 @@ class RenderHTMLData:
                 labels={'x': 'Sweet', 'y': 'Mass'}
             )
 
+        fig.update_traces(marker_color='#ff8d40')
+
+        fig.update_layout(title_x=0.5, title_y=0.9, title_font_size=30, title_font_family='Arial', title_font_color='black', title_font=dict(size=30, family='Arial', color='#4f81bd'))
+
+        fig.update_traces(hoverlabel=dict(bgcolor='white', bordercolor='black', font_size=16, font_family='Arial', font_color='black'))
+
+        fig.update_xaxes(title_font=dict(size=18, family='Arial', color='black'), tickfont=dict(size=14, family='Arial', color='black'))
+
+        fig.update_yaxes(title_font=dict(size=18, family='Arial', color='black'), tickfont=dict(size=14, family='Arial', color='black'))
+
         return fig.to_html()
